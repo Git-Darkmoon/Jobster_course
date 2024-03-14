1feeaea726c3545bf97f712ac98125248b2f9bca
Nicolas Mayorga <namayorgav@gmail.com>
2024-03-14 17:39:45 -0500

    feat: nested routes for the logged user.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import { Landing, Dashboard, Error, Register } from "./pages"
+import { Landing, Error, Register } from "./pages"
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+} from "./pages/dashboard"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 
 const router = createBrowserRouter([
-  { path: "landing", element: <Landing />, errorElement: <Error /> },
   {
     path: "/",
-    element: <Dashboard />,
+    element: <SharedLayout />,
     errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Stats />,
+      },
+      {
+        path: "all-jobs",
+        element: <AllJobs />,
+      },
+      {
+        path: "add-job",
+        element: <AddJob />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
   },
+  { path: "landing", element: <Landing />, errorElement: <Error /> },
   { path: "register", element: <Register />, errorElement: <Error /> },
 ])
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { FormRow, Logo } from "../components"
 import Wrapper from "../assets/wrappers/RegisterPage"
 import { toast } from "react-toastify"
 import { useDispatch, useSelector } from "react-redux"
 import { loginUser, registerUser } from "../features/user/userSlice"
+import { useNavigate } from "react-router-dom"
 
 const initialState = {
   name: "",
@@ -14,9 +15,9 @@ const initialState = {
 
 function Register() {
   const [values, setValues] = useState(initialState)
-
   const { user, isLoading } = useSelector((store) => store.user)
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const handleChange = (e) => {
     const name = e.target.name
@@ -42,6 +43,13 @@ function Register() {
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember })
   }
+
+  useEffect(() => {
+    if (user) {
+      navigate("/")
+    }
+  }, [user, navigate])
+  // navigate was added to de dependency array just for linter warning
 
   return (
     <Wrapper className="full-page">
