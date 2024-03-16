c1f330f96e91ba4a3b3eac021391661a315702c8
Nicolas Mayorga <namayorgav@gmail.com>
2024-03-15 21:09:42 -0500

    add: NavLinks separated component.

---
+++ src/components/NavLinks.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom"
+import links from "../utils/links"
+
+function NavLinks({ toggle }) {
+  return (
+    <div className="nav-links">
+      {links.map((link) => {
+        const { id, text, path } = link
+
+        return (
+          <NavLink
+            key={id}
+            to={path}
+            className="nav-link"
+            activeClassName="active"
+            onClick={toggle}
+          >
+            <span className="icon">{<link.icon />}</span>
+            {text}
+          </NavLink>
+        )
+      })}
+    </div>
+  )
+}
+
+export default NavLinks
--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -2,9 +2,9 @@ import Wrapper from "../assets/wrappers/SmallSidebar"
 import { FaTimes } from "react-icons/fa"
 import { NavLink } from "react-router-dom"
 import Logo from "./Logo"
-import links from "../utils/links"
 import { useSelector, useDispatch } from "react-redux"
 import { toggleSidebar } from "../features/user/userSlice"
+import NavLinks from "./NavLinks"
 
 function SmallSidebar() {
   const { isSidebarOpen } = useSelector((store) => store.user)
@@ -24,23 +24,7 @@ function SmallSidebar() {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">
-            {links.map((link) => {
-              const { id, text, path } = link
-
-              return (
-                <NavLink
-                  key={id}
-                  to={path}
-                  className="nav-link"
-                  activeClassName="active"
-                >
-                  <span className="icon">{<link.icon />}</span>
-                  {text}
-                </NavLink>
-              )
-            })}
-          </div>
+          <NavLinks />
         </div>
       </div>
     </Wrapper>
